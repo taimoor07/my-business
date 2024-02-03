2b12f9dd1fa3a50a4e79b048c3f4c2abd31d9e2a
taimoor07 <dev.taimoor7@gmail.com>
2024-02-03 09:12:58 -0800

    remove the general user module

--- src/modules/users/users.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { registerNewUser } from './users.controller.js';
-const router = express.Router();
-
-router.route('/register').post(registerNewUser);
-
-export default router;
